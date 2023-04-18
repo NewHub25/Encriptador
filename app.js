089dc1def8169cd833e519bf346f6594d1eb7145
Terry Ildefonso <master_correo144@hotmail.com>
2023-04-18 13:54:01 -0500

    Eso es todo

---
+++ app.js
@@ -0,0 +1,35 @@
+const encryptKeys = [
+  ["a", "ai"],
+  ["e", "enter"],
+  ["i", "imes"],
+  ["o", "ober"],
+  ["u", "ufat"],
+];
+function encrypt() {
+  const input = document.getElementById("input").value;
+  document.getElementById("output").value = input.replace(
+    RegExp(encryptKeys.map((m) => m[0]).join("|"), "g"),
+    function (match, offset, string) {
+      return encryptKeys.find((f) => f[0] === match)[1];
+    }
+  );
+}
+function decrypt() {
+  const input = document.getElementById("input").value;
+  document.getElementById("output").value = input.replace(
+    RegExp(encryptKeys.map((m) => m[1]).join("|"), "g"),
+    function (match, offset, string) {
+      return encryptKeys.find((f) => f[1] === match)[0];
+    }
+  );
+}
+function copyText() {
+  navigator.clipboard
+    .writeText(document.getElementById("output").value)
+    .then(() => {
+      console.log("Texto copiado al portapapeles");
+    })
+    .catch((err) => {
+      console.error("Error al copiar el texto: ", err);
+    });
+}
