a30453475378abf9b0da6e06da23258d5703adac
Terry Ildefonso <master_correo144@hotmail.com>
2023-06-14 17:57:40 -0500

    listo para trabajar solo el modal

--- js/background.js
+++ js/background.js
@@ -1,11 +1,12 @@
 const ANIMATED_BACKGROUND = document.getElementById("animatedBackground");
 
 const squares_quantity = 20;
-const range = (a, b) => Math.abs(a - b) * Math.random() + Math.min(a, b);
 const fragment = document.createDocumentFragment();
+
+const range = (a, b) => Math.abs(a - b) * Math.random() + Math.min(a, b);
 for (let i = 0; i < squares_quantity; i++) {
   const square = document.createElement("li");
-  square.style.width = range(20, 150) + "px";
+  square.style.width = range(2, 10) + "vw";
   square.style.aspectRatio = "1/1";
   square.style.left = range(20, 80) + "%";
   square.style.animationDelay = range(0, 20) + "s";
